cc1f3f0b740b03a81d214afddc065238b60f04d7
whyismeleige <piyushjain31456@gmail.com>
2024-07-30 20:53:38 +0530

    website change

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById('enterButton').addEventListener('click',function(){
     window.location.href = 'https://forms.gle/Gg7sHJkCyrb3LgKh9';
 });
+document.getElementById('rulesButton').addEventListener('click',function(){
+    window.location.href = 'rules.html';
+})
+document.getElementById('cluesButton').addEventListener('click',function(){
+    window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0';
+})
